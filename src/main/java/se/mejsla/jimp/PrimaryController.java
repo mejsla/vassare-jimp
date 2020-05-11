daa110d2a9fba7ee5620b909403427f326faff78
Johan Maasing <johan@zoom.nu>
2020-05-11 20:29:29 +0200

    Prepare code-a-long

--- src/main/java/se/mejsla/jimp/PrimaryController.java
+++ src/main/java/se/mejsla/jimp/PrimaryController.java
@@ -3,7 +3,6 @@ package se.mejsla.jimp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -20,5 +19,21 @@ public class PrimaryController {
     @FXML
     public void handleApplicationQuitAction(ActionEvent event) {
         App.getWindow().hide();
+    }
+
+    @FXML
+    public void handleFileOpenAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open file");
+        File file = fileChooser.showOpenDialog(App.getWindow());
+        if (file != null) {
+            try {
+                InputStream imageIS = Files.newInputStream(file.toPath());
+                final Image image = new Image(imageIS);
+                // imageView.setImage(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
